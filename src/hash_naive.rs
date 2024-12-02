b04a4f743a4b194ecc4e462741a3fc025d1a0da1
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2024-12-02 05:31:07 +0530

    Update display

--- src/hash_naive.rs
+++ src/hash_naive.rs
@@ -35,7 +35,7 @@ impl Display for Measurement {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "{}/{}/{}",
+            "{:.1}/{:.1}/{:.1}",
             self.min,
             (self.sum / self.count as f32),
             self.max
